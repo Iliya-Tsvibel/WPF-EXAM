9406ae3141cdf230854e88953571b809c3418926
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-09-01 00:58:29 +0300

    Add files via upload

---
+++ WPF Exam Exes3/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Exam_Exes3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ViewModel viewModel = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            viewModel.GoLoadingTime.Restart();
+            string text = "";
+
+            string newUrl = InsertURL.Text;
+
+            Task.Run(() =>
+            {
+                using (WebClient client = new WebClient())
+                {
+                    text = client.DownloadString(newUrl);
+                }
+
+                int size = ASCIIEncoding.ASCII.GetByteCount(text);
+
+                Action uiwork = () =>
+                {
+                    ShowMeSize.Text = $"The Size Of Current Site Is: {size} Bytes.";
+                };
+
+                SafeInvoke(uiwork);
+                viewModel.GoLoadingTime.Stop();
+            });
+        }
+
+        private void SafeInvoke(Action work)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                work.Invoke();
+                return;
+            }
+            Dispatcher.Invoke(work);
+        }
+
+        private void Counter()
+        {
+            while (viewModel.GoLoadingTime.IsRunning)
+            {
+                SafeInvoke(new Action(() => { ShowMeTime.Text = viewModel.loadingTime.ToString(); }));
+                Thread.Sleep(5);
+            }
+            ShowMeTime.Text = $"This site was loaded during: {viewModel.loadingTime.ToString()} mseconds";
+        }
+
+        
+    }
+}
---
+++ WPF Exam Exes3/ViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPF_Exam_Exes3
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string insertNewUrl;
+        private string newUrl;
+        public string NewUrl { get { return this.newUrl; } set { this.newUrl = value; OnPropertyChanged("NewUrl"); } }
+
+        private string URLSize;
+        public string NewURLSize { get { return URLSize; } set { URLSize = value; OnPropertyChanged("NewURLSize"); } }
+
+        public string loadingTime
+        {
+            get
+            {
+                if (currentLoadingTime.ElapsedMilliseconds == 0) return "0";
+                if (GoLoadingTime.IsRunning == false && currentLoadingTime.ElapsedMilliseconds != 0) return $"The current site was loaded during: {GoLoadingTime.ElapsedMilliseconds.ToString()} milliSeconds";
+                return GoLoadingTime.ElapsedMilliseconds.ToString();
+            }
+            set
+            {
+                OnPropertyChanged("loadingTime");
+            }
+        }
+
+        private Stopwatch currentLoadingTime;
+        public Stopwatch GoLoadingTime { get { return this.currentLoadingTime; } set { this.currentLoadingTime = value; } }
+
+
+        public DelegateCommand StartCommand { get; set; }
+        public bool LoadingIsDone { get; set; } = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ViewModel()
+        {
+            insertNewUrl = "http";
+            newUrl = "";
+            URLSize = "Please Wait...";
+            currentLoadingTime = new Stopwatch();
+
+            StartCommand = new DelegateCommand(() =>
+            {
+                CheckingURL(NewUrl);
+            },
+            () =>
+            {
+                return NewUrl.StartsWith(insertNewUrl);
+            });
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    StartCommand.RaiseCanExecuteChanged();
+                    Thread.Sleep(100);
+                }
+            });
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    loadingTime = GoLoadingTime.ElapsedMilliseconds.ToString();
+                }
+            });
+        }
+
+        public async void CheckingURL(string url)
+        {
+            LoadingIsDone = false;
+            GoLoadingTime.Restart();
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse response = await webRequest.GetResponseAsync();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string numberOfCharacters = await reader.ReadToEndAsync();
+                NewURLSize = $"The Size Of Current Site Is: {numberOfCharacters.Length.ToString()} Bytes.";
+            }
+            LoadingIsDone = true;
+            GoLoadingTime.Stop();
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+
+    }
+}
